4f3016901c7df4c150217513f3a9dafeccf9f8ee
Marcos Ruan <113126971+Marcosruan@users.noreply.github.com>
2026-05-24 16:56:29 -0300

    Metodo atacar do personagem e do inimigo criados

--- src/inimigos/Inimigo.java
+++ src/inimigos/Inimigo.java
@@ -1,11 +1,13 @@
 package inimigos;
 
+import personagem.Personagem;
+
 public class Inimigo {
 	public String nome;
 	public int vida;
 	public int dano;
 	
-	public void atacar() {
-		
+	public void atacar(Personagem jogador) {
+		jogador.vida -= dano;
 	}
 }
--- src/personagem/Personagem.java
+++ src/personagem/Personagem.java
@@ -1,12 +1,20 @@
 package personagem;
 
+import inimigos.Inimigo;
+
 public class Personagem {
 	public String nome;
 	public int vida;
 	public int dano;
 	
-	public void atacar() {
-		
+	public Personagem(String nome, int vida, int dano) {
+		this.nome = nome;
+		this.dano = dano;
+		this.vida = vida;
+	}
+	
+	public void atacar(Inimigo inimigo) {
+		inimigo.vida -= dano;
 	}
 	
 	public void especial() {
