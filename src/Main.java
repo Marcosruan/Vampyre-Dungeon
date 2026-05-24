2d2e2184dc653bfcfccc70aa56b681b4bfca8468
Carlos H <www.carlos4k@gmail.com>
2026-05-24 17:17:33 -0300

    Adicionar comportamentos de ladino

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
 	public static void main(String[] args) {
+		Ladino ladino = new Ladino("Jonnas");
 		
+		System.out.println("Vida: " + ladino.vida + "\nDano: " + ladino.dano);
 	}
 }
--- src/personagem/Ladino.java
+++ src/personagem/Ladino.java
@@ -1,5 +1,20 @@
 package personagem;
 
-public class Ladino {
+public class Ladino extends Personagem {
 
+	public final int vida = 45;
+	public final int dano = 10;
+	boolean escondido = false;
+	
+	
+	public Ladino(String nome) {
+		super(nome, vida, dano);
+	}
+	
+	public void seEsconder() {
+		escondido = true;
+		dano += 10;
+	}
+	
+	
 }
