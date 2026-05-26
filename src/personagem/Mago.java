194d3164f546c89e1af07826d7eab7f7fd189a40
jjonnas-67 <zejonnas.as@gmail.com>
2026-05-26 10:38:09 -0300

    Commit inicial

--- src/personagem/Mago.java
+++ src/personagem/Mago.java
@@ -1,5 +1,17 @@
 package personagem;
 
-public class Mago {
+import inimigos.Inimigo;
 
+public class Mago extends Personagem {
+	
+	static final int VIDABASE = 30;
+	static final int DANOBASE = 15;
+	
+	public Mago(String nome) {
+		super(nome, VIDABASE, DANOBASE);
+	}
+	
+	public void reduzirDanoInimigo(Inimigo inimigo) {
+		inimigo.dano -= 10;
+	}
 }
