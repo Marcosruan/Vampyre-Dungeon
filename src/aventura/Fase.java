f9d111dc83b8b3a0b99c40db8a779d4f83fded1b
MiguelLuan <br.miguelcurseiro1000@gmail.com>
2026-06-03 13:38:49 -0300

    Implementa estrutura inicial da classe Batalha

--- src/aventura/Fase.java
+++ src/aventura/Fase.java
@@ -1,5 +1,16 @@
 package aventura;
+import inimigos.*;
 
 public class Fase {
+    public String introducao;
+    public Inimigo[] inimigosDaFase;
 
+    public Fase(String introducao, Inimigo[] inimigosDaFase) {
+        this.introducao = introducao;
+        this.inimigosDaFase = inimigosDaFase;
+    }
+
+    public String textoIntroducao(){
+        return introducao;
+    }
 }
